7c83910a8509d543e52039a545627cbedf77911d
ChillerDragon <chillerdragon@gmail.com>
2025-12-01 11:29:12 +0100

    Filesystem tests for cmake patcher

--- spec/cmake_spec.rb
+++ spec/cmake_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+
 require_relative '../lib/cmake_patcher'
 
 describe 'CMakePatcher', :array do
@@ -41,6 +43,19 @@ describe 'CMakePatcher', :array do
       patcher = CMakePatcher.new(content: input_cmake)
       patcher.add_file("src/game/server/gamemodes/mymod.cpp")
       expect(patcher.build_new_cmake).to eq(expected_cmake)
+    end
+    it 'Should write to filesystem' do
+      FileUtils.mkdir_p 'spec/tmp'
+      FileUtils.cp('spec/cmake/simple.cmake.in', 'spec/tmp/CMakeLists.txt')
+      patcher = CMakePatcher.new(
+        path: 'spec/tmp/CMakeLists.txt'
+      )
+      patcher.add_file('src/game/server/gamemodes/aaa.cpp')
+      patcher.save
+      expected_cmake = File.read('spec/cmake/simple.cmake.out')
+      got_cmake = File.read('spec/tmp/CMakeLists.txt')
+      FileUtils.rm_rf('spec/tmp')
+      expect(expected_cmake).to eq(got_cmake)
     end
   end
 end
