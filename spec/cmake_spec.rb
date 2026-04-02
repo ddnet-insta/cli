88bd1ec01c28d8a6be513c274f6f7a0420553e4f
ChillerDragon <chillerdragon@gmail.com>
2026-04-02 11:56:40 +0200

    Start to fix tests

--- spec/cmake_spec.rb
+++ spec/cmake_spec.rb
@@ -6,8 +6,8 @@ require_relative '../lib/cmake_patcher'
 
 SIMPLE_CMAKE_CONTENT = <<~EOF
   if(SERVER)
-    set_src(GAME_SERVER GLOB_RECURSE src/game/server
-      gamemodes/vanilla/dm/dm.cpp
+    set_src(INSTA_SERVER GLOB_RECURSE src/insta/server
+        gamemodes/vanilla/dm/dm.cpp
     )
   endif()
 EOF
@@ -36,21 +36,21 @@ describe 'CMakePatcher', :array do
     it 'Should add mymod.cpp' do
       input_cmake = <<~EOF
         if(SERVER)
-          set_src(GAME_SERVER GLOB_RECURSE src/game/server
+          set_src(INSTA_SERVER GLOB_RECURSE src/insta/server
             gamemodes/vanilla/dm/dm.cpp
           )
         endif()
       EOF
       expected_cmake = <<~EOF
         if(SERVER)
-          set_src(GAME_SERVER GLOB_RECURSE src/game/server
-            gamemodes/mymod.cpp
-            gamemodes/vanilla/dm/dm.cpp
+          set_src(INSTA_SERVER GLOB_RECURSE src/insta/server
+              gamemodes/mymod.cpp
+              gamemodes/vanilla/dm/dm.cpp
           )
         endif()
       EOF
       patcher = CMakePatcher.new(content: input_cmake)
-      patcher.add_file('src/game/server/gamemodes/mymod.cpp')
+      patcher.add_file('src/insta/server/gamemodes/mymod.cpp')
       expect(patcher.build_new_cmake).to eq(expected_cmake)
     end
     it 'Should write to filesystem' do
@@ -59,7 +59,7 @@ describe 'CMakePatcher', :array do
       patcher = CMakePatcher.new(
         path: 'spec/tmp/CMakeLists.txt'
       )
-      patcher.add_file('src/game/server/gamemodes/aaa.cpp')
+      patcher.add_file('src/insta/server/gamemodes/aaa.cpp')
       patcher.save
       expected_cmake = File.read('spec/cmake/simple.cmake.out')
       got_cmake = File.read('spec/tmp/CMakeLists.txt')
